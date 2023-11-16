661a4023d0c15bc9e83f1b97e22612cadc34cc82
hovelopin <kimhojin3714@naver.com>
2023-11-16 13:50:12 +0900

    feat : utils 초기 셋팅 완료

---
+++ packages/utils/src/is/isArray/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 배열인지 확인하는 함수
+ * @param arr
+ * @returns {boolean}
+ * @description 이 함수는 1개의 매개변수를 받고 boolean 값을 반환합니다.
+ */
+
+export function isArray(arr: any): boolean {
+  return Array.isArray(arr);
+}
